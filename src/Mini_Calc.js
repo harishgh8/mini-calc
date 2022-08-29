84baf996826d41bab0016afbf977008c874b37c0
Harish <harish@Harishs-Mini.ht.home>
2022-08-29 00:07:02 -0400

    mini calc

---
+++ src/Mini_Calc.js
@@ -0,0 +1,176 @@
+import React, { useState } from "react";
+import style from "./style.css";
+const Mini_calc = () => {
+  const [result, setResult] = useState("");
+  const operators = ["/", "*", "+", "-", "%", "**"];
+  const handleChange = (e) => {
+    let btnValue = e.target.innerText;
+    console.log(btnValue);
+    if (
+      (operators.includes(btnValue) && result === "") ||
+      (operators.includes(btnValue) && operators.includes(result.slice(-1)))
+    ) {
+      return;
+    }
+    if (!operators.includes(btnValue)) {
+      setResult(eval(result + btnValue));
+    }
+
+    setResult(result + btnValue);
+    // if (
+    //   ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(btnValue)
+    // ) {
+    //   setResult(result + btnValue);
+    // } else if (btnValue === "C" && result !== "") {
+    //   let tempResults = result.toString().slice(0, -1);
+    //   setResult(tempResults);
+    // } else if (btnValue === "AC" && result !== "") {
+    //   setResult("");
+    // } else if (
+    //   ["/", "*", "+", "-", "%", "**"].includes(btnValue) &&
+    //   result !== ""
+    // ) {
+    //   setResult(result + btnValue);
+    // }
+  };
+  const calculate = () => {
+    setResult(eval(result).toString());
+  };
+  const del = () => {
+    if (result == "") {
+      return;
+    }
+    const value = result.slice(0, -1);
+    setResult(value);
+  };
+  const clear = () => {
+    setResult("");
+  };
+  return (
+    <div className="app">
+      <header>Calculator</header>
+      <div className="container">
+        <div className="display">{result}</div>
+        <div className="keypad">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <button className="btn-blue" value="AC" onClick={clear}>
+                    AC
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-blue" onClick={del}>
+                    C
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-blue" onClick={handleChange}>
+                    %
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-blue" onClick={handleChange}>
+                    /
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    7
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    8
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    9
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-blue" onClick={handleChange}>
+                    {" "}
+                    *
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    4
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    5
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    6
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-blue" onClick={handleChange}>
+                    -
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    1
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    2
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    3
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-blue" onClick={handleChange}>
+                    +
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="btn-black" onClick={handleChange}>
+                    .
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-cyan" onClick={handleChange}>
+                    0
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-red" onClick={calculate}>
+                    =
+                  </button>
+                </td>
+                <td>
+                  <button className="btn-blue" onClick={handleChange}>
+                    **
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mini_calc;
